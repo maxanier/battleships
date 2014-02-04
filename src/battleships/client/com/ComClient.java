26056664f3cf9eebfd67ca0d0f3efe758c36a601
Maxanier <maxanier@gmx.de>
2014-02-04 16:36:04 +0100

    Structure for communication within the client

--- src/battleships/client/com/ComClient.java
+++ src/battleships/client/com/ComClient.java
@@ -2,9 +2,9 @@ package battleships.client.com;
 
 import battleships.abiklassen.enhanced.EnhancedClient;
 
-public class BattleshipClient extends EnhancedClient {
+public class ComClient extends EnhancedClient {
 
-	public BattleshipClient(String pIPAdresse, int pPortNr) {
+	public ComClient(String pIPAdresse, int pPortNr) {
 		super(pIPAdresse, pPortNr);
 		// TODO Auto-generated constructor stub
 	}
