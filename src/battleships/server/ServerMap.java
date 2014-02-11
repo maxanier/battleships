2eec6daa2f307052f9f310a63884976e2a49e0f9
Maxanier <maxanier@gmx.de>
2014-02-11 15:46:34 +0100

    ServerMap

---
+++ src/battleships/server/ServerMap.java
@@ -0,0 +1,122 @@
+package battleships.server;
+
+import battleships.util.FieldId;
+
+public class ServerMap {
+	int[][] map;
+	
+	
+	/**
+	 * Creates a x*y map and fills it with the given id
+	 * @param x x-size
+	 * @param y y-size
+	 * @param standard Default Id
+	 */
+	private ServerMap(int x,int y,int standard){
+		map = new int[x][y];
+		
+		if(standard!=0){
+			for(int i=0;i<x;i++){
+				for(int j=0;j<y;j++){
+					map[i][j]=standard;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Creates a new ServerMap from String
+	 * @param s
+	 * @return
+	 */
+	public static ServerMap createFromString(String s,int x,int y){
+		if(x<1||y<1){
+		return null;
+		}
+		ServerMap serverMap=new ServerMap(x,y,FieldId.WATER);
+		
+		
+		return serverMap;
+	}
+	
+	/**
+	 * Checks if the ships are placed correctly
+	 * @return Valid map
+	 */
+	public boolean isValid(){
+		return true;
+	}
+
+	/**
+	 * Shoots at the given Location. If a shippart is hit, it is set to sunk.
+	 * @param x X-Coord
+	 * @param y Y-Coord
+	 * @return Hit
+	 */
+	public boolean shoot(int x,int y){
+		if(map[x][y]==FieldId.SHIP){
+			map[x][y]=FieldId.SUNKEN_SHIP;
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks if there is a shippart in the four adjacent cells.
+	 * Can be used to check if a ship is completly destroyed 
+	 * @param x X-Coord
+	 * @param y Y-Coord
+	 * @return Ship nearby
+	 */
+	public boolean shipNearby(int x,int y){
+		try{
+			if(map[x-1][y]==FieldId.SHIP){
+				return true;
+			}
+		}
+		catch(IndexOutOfBoundsException e){
+			
+		}
+		try{
+			if(map[x+1][y]==FieldId.SHIP){
+				return true;
+			}
+		}
+		catch(IndexOutOfBoundsException e){
+			
+		}
+		try{
+			if(map[x][y-1]==FieldId.SHIP){
+				return true;
+			}
+		}
+		catch(IndexOutOfBoundsException e){
+			
+		}
+		try{
+			if(map[x][y+1]==FieldId.SHIP){
+				return true;
+			}
+		}
+		catch(IndexOutOfBoundsException e){
+			
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks if there are any ship parts left, which are not sunk.
+	 * @return Ships left
+	 */
+	public boolean shipsLeft(){
+		for(int a[] : map){
+			for(int b : a){
+				if(b==FieldId.SHIP){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+}
---
+++ src/battleships/util/FieldId.java
@@ -0,0 +1,9 @@
+package battleships.util;
+
+public class FieldId {
+	
+	public static final int UNKNOWN=-1;
+	public static final int WATER=0;
+	public static final int SHIP=1;
+	public static final int SUNKEN_SHIP=2;
+}
