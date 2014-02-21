74c2cd2a18dafae9a0413d36dabdab1e2dad9096
maxanier <maxanier@gmx.de>
2014-02-21 04:55:28 -0300

    Changed UNKNOWN ID to be positive

--- src/battleships/util/FieldId.java
+++ src/battleships/util/FieldId.java
@@ -2,7 +2,7 @@ package battleships.util;
 
 public class FieldId {
 
-	public static final int UNKNOWN = -1;
+	public static final int UNKNOWN = 3;
 	public static final int WATER = 0;
 	public static final int SHIP = 1;
 	public static final int SUNKEN_SHIP = 2;
