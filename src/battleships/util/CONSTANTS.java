a1b086a6cda891a598543bfa9dc67b0e9474c105
maxanier <maxanier@gmx.de>
2014-02-21 05:20:42 -0300

    Added game size and made everything final

--- src/battleships/util/CONSTANTS.java
+++ src/battleships/util/CONSTANTS.java
@@ -2,6 +2,7 @@ package battleships.util;
 
 public class CONSTANTS {
 	public static final String LOG_DIRECTORY = "";
-	public static int PORT = 6000;
-	public static String LOG_FILE_NAME = "log.txt";
+	public static final int PORT = 6000;
+	public static final String LOG_FILE_NAME = "log.txt";
+	public static final int GAME_SIZE=10;
 }
