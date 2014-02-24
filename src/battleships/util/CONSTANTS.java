6dbbfb0bd05c2a6e40fb55713cf0f8d04e97ac1e
maxanier <maxanier@gmx.de>
2014-02-24 04:35:39 -0300

    Implemented getShipAmounts

--- src/battleships/util/CONSTANTS.java
+++ src/battleships/util/CONSTANTS.java
@@ -1,8 +1,20 @@
 package battleships.util;
 
+import javax.swing.JButton;
+
 public class CONSTANTS {
 	public static final String LOG_DIRECTORY = "";
 	public static final int PORT = 6000;
 	public static final String LOG_FILE_NAME = "log.txt";
 	public static final int GAME_SIZE=10;
+	
+	public static int[] getShipAmount(){
+		int [] shipAmounts = new int[5];
+		for(int i=0; i<3; i++)
+			shipAmounts[i] = GAME_SIZE/10;
+		for(int i=3;i<5;i++)
+			shipAmounts[i] = GAME_SIZE/5;
+		
+		return shipAmounts;
+	}
 }
